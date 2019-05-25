c31f9ea4e148a2d40a2fc559964835658c6f4c38
Johann Muszynski <johann.muszynski@wolt.com>
2019-05-25 10:13:33 +0200

    Build C code directly in main exe target

--- build.zig
+++ build.zig
@@ -3,15 +3,10 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const lib_cflags = [][]const u8{"-std=c99"};
 
-    var sdl_wrapper = b.addStaticLibrary("pixel", null);
-    sdl_wrapper.addCSourceFile("src/pixel.c", lib_cflags);
-    sdl_wrapper.addIncludeDir("src/");
-    sdl_wrapper.addIncludeDir("/Library/Frameworks/SDL2.framework/Headers");
-
     const exe = b.addExecutable("zig-tracer", "src/main.zig");
     exe.setBuildMode(b.standardReleaseOptions());
+    exe.addCSourceFile("src/pixel.c", lib_cflags);
     exe.addIncludeDir("src/");
-    exe.linkLibrary(sdl_wrapper);
     exe.linkSystemLibrary("c");
     if (exe.target.isDarwin()) {
         exe.addIncludeDir("/Library/Frameworks/SDL2.framework/Headers");
