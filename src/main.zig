d234d6b8e07e29f750e9e60666a80198fc2b4ecf
makoto abe <abe00makoto@gmail.com>
2022-07-10 18:41:26 +0900

    fix compile error

--- src/main.zig
+++ src/main.zig
@@ -55,7 +55,7 @@ fn colorAlbedo(r: Ray, w: *const World) Vec3f {
         return switch (hit.material) {
             Material.Lambertian => |l| l.albedo,
             Material.Metal => |m| m.albedo,
-            Material.Dielectric => |l| Vec3f.one(),
+            Material.Dielectric => Vec3f.one(),
         };
     } else {
         const unit_direction = r.direction.makeUnitVector();
@@ -150,11 +150,11 @@ fn renderFn(context: *ThreadContext) void {
         var color_accum = Vec3f.zero();
 
         while (sample < num_samples) : (sample += 1) {
-            const v = (@intToFloat(f32, h) + context.rng.random.float(f32)) / @intToFloat(f32, window_height);
-            const u = (@intToFloat(f32, w) + context.rng.random.float(f32)) / @intToFloat(f32, window_width);
+            const v = (@intToFloat(f32, h) + context.rng.random().float(f32)) / @intToFloat(f32, window_height);
+            const u = (@intToFloat(f32, w) + context.rng.random().float(f32)) / @intToFloat(f32, window_width);
 
-            const r = context.camera.makeRay(&context.rng.random, u, v);
-            const color_sample = color(r, context.world, &context.rng.random, 0);
+            const r = context.camera.makeRay(&context.rng.random(), u, v);
+            const color_sample = color(r, context.world, &context.rng.random(), 0);
             // const color_sample = colorScattering(r, context.world, &context.rng.random);
             // const color_sample = colorDepth(r, context.world, &context.rng.random);
             // const color_sample = colorNormal(r, context.world);
@@ -203,7 +203,6 @@ pub fn main() !void {
     try world.spheres.append(Sphere.new(Vec3f.new(4.0, 1.0, 0.0), 1.0, Material.metal(Vec3f.new(0.7, 0.6, 0.5), 0.0)));
 
     var prng = rand.DefaultPrng.init(0);
-
     const sphere_offset = Vec3f.new(4.0, 0.2, 0.0);
     var i: i32 = -5;
     while (i < 5) : (i += 1) {
@@ -211,17 +210,17 @@ pub fn main() !void {
         while (j < 5) : (j += 1) {
             const a = @intToFloat(f32, i);
             const b = @intToFloat(f32, j);
-            const center = Vec3f.new(a + 0.9 * prng.random.float(f32), 0.2, b + 0.9 * prng.random.float(f32));
-            const choose_mat = prng.random.float(f32);
+            const center = Vec3f.new(a + 0.9 * prng.random().float(f32), 0.2, b + 0.9 * prng.random().float(f32));
+            const choose_mat = prng.random().float(f32);
             if (center.sub(sphere_offset).length() > 0.9) {
                 if (choose_mat < 0.8) {
                     // diffuse
-                    const random_albedo = Vec3f.new(prng.random.float(f32), prng.random.float(f32), prng.random.float(f32));
+                    const random_albedo = Vec3f.new(prng.random().float(f32), prng.random().float(f32), prng.random().float(f32));
                     try world.spheres.append(Sphere.new(center, 0.2, Material.lambertian(random_albedo)));
                 } else if (choose_mat < 0.95) {
                     // metal
-                    const random_albedo = Vec3f.new(prng.random.float(f32), prng.random.float(f32), prng.random.float(f32));
-                    try world.spheres.append(Sphere.new(center, 0.2, Material.metal(random_albedo, 0.5 * prng.random.float(f32))));
+                    const random_albedo = Vec3f.new(prng.random().float(f32), prng.random().float(f32), prng.random().float(f32));
+                    try world.spheres.append(Sphere.new(center, 0.2, Material.metal(random_albedo, 0.5 * prng.random().float(f32))));
                 } else {
                     try world.spheres.append(Sphere.new(center, 0.2, Material.dielectric(1.5)));
                 }
@@ -260,9 +259,11 @@ pub fn main() !void {
                     .world = &world,
                     .camera = &camera,
                 });
-                const thread = try std.Thread.spawn(&contexts.items[@intCast(usize, ithread)], renderFn);
+                // .{contexts.items[@intCast(usize, ithread)]
+                // const config = std.Thread.SpawnConfig;
+                const thread = try std.Thread.spawn(.{}, renderFn, .{&contexts});
 
-                try tasks.append(thread);
+                try tasks.append(&thread);
             }
         }
 
@@ -293,3 +294,5 @@ pub fn main() !void {
         c.SDL_Delay(16);
     }
 }
+
+
